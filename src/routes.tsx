43f484f4d533a8676ced1f5c159ce45a54756095
Victor-Gabriel-marinho <victorgab0802@gmail.com>
2025-09-03 10:31:03 -0300

    atualizando frontend(tela times + modal incompleto)

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Cadastro from "./pages/cadastro/cadastro"
 import Login from "./pages/login/entrar";
+import Times from "./pages/times/times";
 
 function RoutesApp() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Cadastro/>}/>
+                <Route path="/cadastro" element={<Cadastro/>}/>
+                <Route path="/" element={<Times/>}/>
                 <Route path="/entrar" element={<Login/>}/>
             </Routes>
         </BrowserRouter>
