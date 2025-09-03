54f58fd5321bdca35a405b89c6a9e4318a96ec91
Wladson14 <wladsonjacinto@gmail.com>
2025-09-03 10:35:10 -0300

    tela de criação de quadros

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Cadastro from "./pages/cadastro/cadastro"
 import Login from "./pages/login/entrar";
+import Quadros from "./pages/quadros/quadros";
 
 function RoutesApp() {
     return (
@@ -8,6 +9,7 @@ function RoutesApp() {
             <Routes>
                 <Route path="/" element={<Cadastro/>}/>
                 <Route path="/entrar" element={<Login/>}/>
+                 <Route path="/quadros" element={<Quadros/>}/>
             </Routes>
         </BrowserRouter>
     )
