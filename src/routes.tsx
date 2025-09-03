dfc7d393dc2c9a404bc692d3de7fc1542e2cfdd0
Wladson14 <wladsonjacinto@gmail.com>
2025-09-03 10:35:10 -0300

    tela de criação de quadros

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,16 +3,22 @@ import Cadastro from "./pages/cadastro/cadastro";
 import Login from "./pages/login/entrar";
 import Quadros from "./pages/quadros/quadros";
 import Times from "./pages/times/times";
+<<<<<<< HEAD
 import Lista from "./pages/lista-tarefas/lista";
 import Layout from "./pages/times/components/layout/layout";
 import People from "./pages/times/People";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { BlockRouter } from "./components/BlockRoute";
+=======
+import Quadros from "./pages/quadros/quadros";
+
+>>>>>>> d592cd4 (tela de criação de quadros)
 
 function RoutesApp() {
     return (
         <BrowserRouter>
             <Routes>
+<<<<<<< HEAD
                 <Route element={<BlockRouter />}>
                     <Route path="/cadastro" element={<Cadastro />} />
                     <Route path="/entrar" element={<Login />} />
@@ -26,6 +32,12 @@ function RoutesApp() {
                         <Route path="/lista" element={<Lista />} />
                         <Route path="/quadros" element={<Quadros />} />
                 </Route>
+=======
+                <Route path="/cadastro" element={<Cadastro/>}/>
+                <Route path="/" element={<Times/>}/>
+                <Route path="/entrar" element={<Login/>}/>
+                 <Route path="/quadros" element={<Quadros/>}/>
+>>>>>>> d592cd4 (tela de criação de quadros)
             </Routes>
         </BrowserRouter>
     );
