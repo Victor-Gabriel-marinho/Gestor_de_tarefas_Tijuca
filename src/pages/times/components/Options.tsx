d5634add715e3348c0251b233011efee912c8b61
Victor-Gabriel-marinho <victorgab0802@gmail.com>
2025-09-17 10:41:57 -0300

    resolvendo conflitos

--- src/pages/times/components/Options.tsx
+++ src/pages/times/components/Options.tsx
@@ -29,15 +29,6 @@ function Options({id, children }: OptionsProps) {
       console.log('erro ao fazer requisição', error)
     }
   } 
-
-<<<<<<< HEAD
-    
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 2645210 (terminando funcionalidades)
-=======
->>>>>>> feature/integrando_com_temaService_da_API
   return (
     <div className="bg-[#251F1F] w-full h-[150px] sm:h-[173px] rounded-b-[20px] rounded-t-[10px] flex flex-col">
       {children}
@@ -56,29 +47,7 @@ function Options({id, children }: OptionsProps) {
       </div>
     </div>
   );
-<<<<<<< HEAD
-}
-
-export default Options;
-<<<<<<< HEAD
-=======
-    return (
-        <div className="bg-[#251F1F] w-full h-[150px] sm:h-[173px] rounded-b-[20px] rounded-t-[10px] flex flex-col">
-            {children}
-            <div className="w-full h-[94px] flex items-center justify-around">
-                <input type="button" value="Remover" className=" w-[80px] sm:w-[250px] h-[38px] text-sm sm:text-xl text-white bg-[#F21223] cursor-pointer rounded-[10px]" />
-                <input type="button" value="Promover" className="w-[80px] sm:w-[250px] h-[38px] text-sm sm:text-xl text-white bg-[#076F37] cursor-pointer rounded-[10px]" />
-            </div>
-        </div>
-    )
 }
 
 export default Options
->>>>>>> 53acb9c (ajustando funcionalidades do frontend)
-=======
-}
 
-export default Options;
->>>>>>> 2645210 (terminando funcionalidades)
-=======
->>>>>>> feature/integrando_com_temaService_da_API
