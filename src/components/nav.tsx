b0c67c87a5d1bce43a361e969eadb09615629301
Wladson14 <wladsonjacinto@gmail.com>
2025-09-18 09:33:04 -0300

    reajuste para impedir conflitos

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,20 +1,49 @@
 import { MdPeople } from "react-icons/md";
 import { BsFillHouseFill } from "react-icons/bs";
 import { IoFilter } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import Filtrar from "./filtro";
+import { useState } from "react";
 
 function Nav() {
+
+    const [filtrartask, Setfiltrartask] = useState<boolean>(false)
+
    return (
    <nav className="bg-[#524D50] w-screen h-10 ">
 
         <div className="flex gap-4 p-1 justify-between">
-            <BsFillHouseFill className="cursor-pointer" color="white" size={30}/>
+            <Link to="/quadros">
+                <BsFillHouseFill className="cursor-pointer" color="white" size={30}/>
+            </Link>
             <div className="flex gap-4">
-                <IoFilter className="cursor-pointer" color="white" size={30}/>
-                <MdPeople className="cursor-pointer" color="white" size={30}/>
-            </div>
-        </div>
+                <div className="relative">
+                    <IoFilter className="cursor-pointer" color="white" size={30} onClick={() => Setfiltrartask(!filtrartask)}/>
+                    
+                    {filtrartask && (
+                     <div className="relative">
+                            
+                        <Filtrar/>
+                        
+                    </div>
+                )}
+                        
+                        
+                </div>
+                <Link to="/">
+                    <MdPeople className="cursor-pointer" color="white" size={30}/>
+                </Link>
 
+                 
+            </div>
+
+
+        </div >
+           
     </nav>
+
+    
+    
    )
 }
 
