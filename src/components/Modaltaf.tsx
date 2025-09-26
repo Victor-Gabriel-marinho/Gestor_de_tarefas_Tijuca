65be78c047e2a8592ecf3f4c1619ff64c3c4db1e
Wladson14 <wladsonjacinto@gmail.com>
2025-09-26 08:45:38 -0300

    desenvolvimento das funções de adicionar tag, anexo, exclusão, editar no modal

--- src/components/Modaltaf.tsx
+++ src/components/Modaltaf.tsx
@@ -1,80 +1,191 @@
+{/*ícones usados no modal*/ }
 import { IoIosClose } from "react-icons/io"
 import { FaClock } from "react-icons/fa"
 import { GoPaperclip } from "react-icons/go"
 import { IoMdPricetag } from "react-icons/io"
 import { FaTrashCan } from "react-icons/fa6"
 import { GoPaperAirplane } from "react-icons/go"
+import { MdEdit } from "react-icons/md";
 import { useFont } from "./font"
-import { useState } from "react"
+import { useState, useRef, useEffect} from "react"
+import Tag from "./tags" 
 
-function Modaltaf({ task, onClose }: { task: string, onClose: () => void }) {
+{/*recebe uma tarefa e uma função para fechar o modal*/ }
+function Modaltaf({ task, onClose, onDelete, onUpdateTask }: { task: string, onClose: () => void; onDelete: () => void, onUpdateTask: (novoNome: string) => void }) {
 
     useFont(" 'Poppins', 'SansSerif' ");
 
-    const [date, Setdate] = useState<boolean>(false)
-    const [prazo, Setprazo] = useState<string>("")
-    const hoje = new Date()
-    let mensagem = ""
+    {/*controle do input type date*/ }
 
-    if (new Date(prazo) < hoje) {
-        mensagem = "Atrasada"   
-    } else if (new Date(prazo) == hoje) {
-        mensagem = "Vence hoje"
-    } else  {
-        mensagem = "Normal"
+    {/*upload de arquivos*/ }
+    const [file, Setfile] = useState<File | null>(null) //arquivo que foi selecionado
+    const [filename, Setfilename] = useState<string | null>(null) //nome do arquivo
+    {/*mostra o preview de .img na tela*/ }
+    const [preview, Setpreview] = useState<string | null>(null)
+    const [zoomimg, Setzoomimg] = useState<string | null>(null)
+    const inputfile = useRef<HTMLInputElement | null>(null) //referencia para o input type file
+
+    {/*abre o seletor de arquivos ao clicar no botão com o clip*/ }
+    const selectfile = () => {
+        if (inputfile.current) {
+            inputfile.current.click()
+        }
     }
-        
-    console.log(prazo)
+
+    const [tag, Settag] = useState<boolean>(false)
+    const [tagvalue, Settagvalue] = useState<string>("")
+
+    const [edit, Setedit] = useState<boolean>(false)
+    const [edittask, Setedittask] = useState<string>(task)
+    useEffect(() =>{
+        Setedittask(task);
+    }, [task])
+
 
     return (
         <>
 
             <div className="w-screen h-screen bg-black/50 flex items-center justify-center  fixed top-0 left-0 right-0 backdrop-blur-[20px]">
-                <div className="bg-[#251F1F] h-50 w-80 rounded-[10px] shadow-2xl text-white relative p-10">
-                    <button className="absolute top-2 right-2 cursor-pointer" onClick={onClose}>
-                        <IoIosClose size={40} />
-                    </button>
-                    <div className="flex gap-1 flex-col h-20">
+                {/*caixa do modal*/}
+                <div className="bg-[#251F1F] max-w-[90vw] max-h-[90vh] overflow-auto rounded-[10px] shadow-2xl text-white relative p-10 flex flex-col">
+                    <div className="flex justify-end">
+                        {/*botão para fechar o modal*/}
+                        <button className="absolute top-4 right-4 cursor-pointer " onClick={onClose}>
+                            <IoIosClose size={40} />
+                        </button>
+                    </div>
+                    <div className="flex gap-2 flex-col">
 
-                        <div className="flex flex-row gap-2">
+                        <div className="flex gap-2 items-center">
                             <input type="checkbox" name="" className="accent-[#22C55E]" />
-                            <p className="truncate">{task}</p>
+                            <p className="truncate max-w-60">{edit ? edittask : task}</p>
+                            <MdEdit className="m-1" onClick={() => Setedit(!edit)}/>
                         </div>
 
-                        <div>
-                            {date && (
-                                <input type="date" name="" id="" className="absolute z-50 mt-25 outline-none"
-                                value={prazo}
-                                onChange={(e) => Setprazo(e.target.value)}
-                                />
-                                
+                        <div className="space-y-2">
+
+
+                            {/*input escondido que é aberto pelo botão com clip*/}
+                            <input type="file" name="" id="" className="absolute z-50 mt-50 outline-none hidden"
+                                ref={inputfile} //referência ao input type file
+                                accept="image/*,.pdf,.doc,.docx" //tipos de arquivos que são aceitos 
+                                onChange={(e) => {
+                                    if (e.target.files && e.target.files[0]) {
+                                        const f = e.target.files[0]
+                                        Setfile(f)
+                                        Setfilename(f.name)
+
+                                        //se for imagem, cria o preview
+                                        if (f.type.startsWith("image/")) {
+                                            Setpreview(URL.createObjectURL(f))
+                                        } else {
+                                            Setpreview(null)
+                                        }
+                                    }
+                                }}
+                            />
+
+                            {/* bara de botões*/}
+                            <div className=" top-[70px] flex gap-3 p-1">
+                                <FaClock className="hover:scale-110" size={30} />
+                                {/*botão responsável por ativar o input type file*/}
+                                <button onClick={selectfile}>
+                                    <GoPaperclip className="hover:scale-110" size={30} />
+                                </button>
+                                <IoMdPricetag className="hover:scale-110" size={30} onClick={() => Settag(!tag)} />
+                                <FaTrashCan className="hover:scale-110" color="red" size={30} onClick={() => {
+                                    onDelete();
+                                    onClose();
+                                }
+                                } />
+
+                            </div>
+
+                            {tagvalue && (
+                                <div className="m-1 ">
+                                    <span className="text-white bg-blue-500 p-1 rounded-[5px]">
+                                        {tagvalue}
+                                    </span>
+                                </div>
                             )}
 
-                            
 
-                            <div className="absolute top-[70px] flex gap-3 p-1">
-                                <FaClock className="hover:scale-110" size={30} onClick={() => Setdate(!date)}/>
-                                <GoPaperclip className="hover:scale-110" size={30} />
-                                <IoMdPricetag className="hover:scale-110" size={30} />
-                                <FaTrashCan className="hover:scale-110" color="red" size={30} />
-                            </div>
-                            <div className="flex items-center gap-2 mt-15">
-                                <input type="text" placeholder="Adicionar comentário" className="absolute w-50 left-10 bg-white outline-none placeholder-gray-400 text-black rounded-[5px]  ring-1 ring-gray-300 focus:ring-2 focus:ring-blue-500 truncate" />
+                            <div className="flex items-center gap-2 mt-5">
+                                <form action="" className="flex gap-2">
+                                    <input type="text" placeholder="Adicionar comentário" className=" w-50 left-10 bg-white outline-none placeholder-gray-400 text-black rounded-[5px]  ring-1 ring-gray-300 focus:ring-2 focus:ring-blue-500 truncate" />
+                                    <button className=" w-[30px] h-[30px] p-1 left-[245px] bg-[#4b3f3f] hover:bg-[#574848] rounded-[10px] flex items-center justify-center"><GoPaperAirplane /></button>
+                                </form>
 
-                                <button className="absolute w-[30px] h-[30px] p-1 left-[245px] bg-[#4b3f3f] hover:bg-[#574848] rounded-[10px] flex items-center justify-center"><GoPaperAirplane /></button>
                             </div>
-                            {mensagem && <p className={`p-4 ${
-                                mensagem === "Atrasada"
-                                ? "text-red-600"
-                                : mensagem === "Vence hoje"
-                                ?"text-yellow-600"
-                                :"text-green-600"
-                            }`}>{mensagem}
-                            </p>}
+
+                            {edit &&(
+                            <div className="flex gap-2">
+                                <input
+                                    type="text"
+                                    value={edittask} 
+                                    onChange={(e) => Setedittask(e.target.value)}
+                                    className="bg-white text-black p-1 rounded outline-none w-50 left-10 truncate"
+                                />
+                                <button className="bg-[#4b3f3f]  px-2 rounded" onClick={() => 
+                                {
+                                    if (edittask.trim() === "") {
+                                        Setedittask(task)
+                                    } else {
+                                        onUpdateTask(edittask)
+                                    } 
+                                    Setedit(false)
+                                    onClose()
+                                   
+                                }}
+                                        
+                                >Editar</button>
+                            </div>
+                            )}
+                            {/*imprimi o nome do arquivo na tela*/}
+                            {filename && (
+                                <div className="flex flex-col items-start gap-2">
+                                    <p className="text-sm truncate">{filename}</p>
+                                    {preview && (
+                                        <img src={preview} alt="" className="w-20 cursor-pointer"
+                                            onClick={() => Setzoomimg(preview)}
+                                        />
+                                    )}
+                                </div>
+                            )}
+
+                            {zoomimg && (
+                                <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
+                                    onClick={() => Setzoomimg(null)}
+                                >
+                                    <img src={zoomimg} alt="" className="max-w-[90%] max-h-[90%] rounded-lg" />
+                                </div>
+                            )}
+
+                            {/*mostra o nome do arquivo pdf como link*/}
+                            {file?.type === "application/pdf" && (
+                                <a href={URL.createObjectURL(file)}
+                                    target="_blank"
+                                    rel="noopener noreferrer" //por segurança
+                                    type="application/pdf"
+                                    className="text-blue-400 underline"
+                                >{filename}</a>
+                            )}
+
                         </div>
-                            
+
                     </div>
+
                 </div>
+
+                {tag && (
+                    <Tag
+                        onDefinir={(tag) => {
+                            Settagvalue(tag)
+                            Settag(false)
+                        }}
+                    />
+                )}
+
             </div>
 
         </>
--- src/pages/lista-tarefas/lista.tsx
+++ src/pages/lista-tarefas/lista.tsx
@@ -6,49 +6,39 @@ import { FiMinimize2 } from "react-icons/fi";
 import Filtrar from "../../components/filtro";
 
 
-
 function Lista() {
 
-    
-   
-
-
-  
-
-
-
-
-    {/*hook para trazer a fonte*/}
+    {/*hook para trazer a fonte*/ }
     useFont(" 'Poppins', 'SansSerif' ");
 
-    {/*constantes para o input e a tarefa da lista pendente*/}
+    {/*constantes para o input e a tarefa da lista pendente*/ }
     const [inputpen, Setinputpen] = useState<boolean>(false)
     const [tarefapen, Settarefapen] = useState<string>("")
 
-    {/*constantes para o input e a tarefa da lista em progresso*/}
+    {/*constantes para o input e a tarefa da lista em progresso*/ }
     const [inputprog, Setinputprog] = useState<boolean>(false)
     const [tarefaprog, Settarefaprog] = useState<string>("")
 
-    {/*constantes para o input e a tarefa da lista concluídas*/}
+    {/*constantes para o input e a tarefa da lista concluídas*/ }
     const [inputconc, Setinputconc] = useState<boolean>(false)
     const [tarefaconc, Settarefaconc] = useState<string>("")
 
-    {/*constantes para criar nova lista e guardar o seu nome*/}
+    {/*constantes para criar nova lista e guardar o seu nome*/ }
     const [novaListaInput, setNovaListaInput] = useState<string>("")
     const [novalista, Setnovalista] = useState<boolean>(false)
     const [nomelista, Setnomelista] = useState<string>("")
 
-    {/*constantes para criar uma nova tarefa para ela*/}
+    {/*constantes para criar uma nova tarefa para ela*/ }
     const [novatarefa, Setnovatarefa] = useState<boolean>(false)
     const [tarefaNovaLista, SettarefaNovaLista] = useState<string>("")
 
-    {/*constantes para para chamar o componente modal_taf para a tarefa selecionada*/}
+    {/*constantes para para chamar o componente modal_taf para a tarefa selecionada*/ }
     const [modaltask, Setmodaltask] = useState<boolean>(false)
     const [select, Setselect] = useState<string>("")
-    
-    
 
-    {/*objeto para minimizar as listas*/}
+
+
+    {/*objeto para minimizar as listas*/ }
     const [minimize, setMinimize] = useState({
         pendente: false,
         fazendo: false,
@@ -56,10 +46,12 @@ function Lista() {
         nova: false,
     });
 
-    {/*constante que necessita do objeto para minimizar a tarefa*/}
+    {/*constante que necessita do objeto para minimizar a tarefa*/ }
     const toggleMinimize = (key: keyof typeof minimize) => {
         setMinimize(prev => ({ ...prev, [key]: !prev[key] }))
     }
+
+
 
     return (
         <>
@@ -67,34 +59,41 @@ function Lista() {
                 {/*navbar da página*/}
                 <Nav>
                     <Filtrar />
-                    
                 </Nav>
 
                 <main className=" flex  flex-col md:flex-row gap-5 md:gap-10 m-5 items-center justify-center">
                     <div className="flex items-center justify-center flex-col gap-5 w-10/12 sm:flex-row ">
                         <div className="bg-[#251F1F] text-center p-3 rounded-[5px] flex flex-col w-full h-full gap-y-2 max-w-60">
                             {/*lista de tarefas pendente*/}
-                            <div className="flex items-center justify-center">
-                                <p className="text-white font-semibold fixed">Pendente</p>
+                            <div className="flex items-center justify-between ">
+                                <p className="text-white font-semibold flex-1 text-center">Pendente</p>
+
 
                                 <div className="flex justify-end p-1 rounded-[15px]">
                                     {/*icone para minimizar tarefa*/}
-                                    <FiMinimize2 color="white" className="hover:scale-125 ml-[170px]"
+                                    <FiMinimize2 color="white" className="hover:scale-125  cursor-pointer"
                                         onClick={() => toggleMinimize("pendente")}
                                     />
                                 </div>
                             </div>
 
 
-  
-   
-  
-                            {/*função minimizar do pendente*/}                           
+
+
+
+                            {/*função minimizar do pendente*/}
                             {!minimize.pendente && (
                                 <>
-                                
-                                    <p className="bg-white cursor-pointer h-[35px] p-1 text-center rounded-[5px]" onClick={() => { Setselect("Começar API"); Setmodaltask(true) }}>Começar API</p>
-                                    <p className={`bg-white cursor-pointer truncate text-center p-1 rounded-[5px] ${tarefapen ? 'block h-[35px]' : 'hidden'}`} onClick={() => { Setselect(tarefapen); Setmodaltask(true) }}>{tarefapen}</p>
+
+                                    <div>
+
+
+                                        <p className="bg-white cursor-pointer h-[35px] p-1 text-center rounded-[5px]" onClick={() => { Setselect("Começar API"); Setmodaltask(true) }}>Começar API</p>
+                                    </div>
+                                    <div>
+
+                                        <p className={`bg-white cursor-pointer truncate text-center p-1 rounded-[5px] ${tarefapen ? 'block h-[35px]' : 'hidden'}`} onClick={() => { Setselect(tarefapen); Setmodaltask(true) }}>{tarefapen}</p>
+                                    </div>
                                     {inputpen && (<textarea placeholder="nome" className="bg-white outline-none placeholder-gray-400 h-[35px] resize-none"
                                         value={tarefapen}
                                         onChange={(e) => Settarefapen(e.target.value)}
@@ -110,12 +109,12 @@ function Lista() {
                         </div>
                         <div className="bg-[#251F1F] text-center p-3 rounded-[5px] flex flex-col w-full h-full gap-y-2 max-w-60">
                             {/*lista de terfas em progresso*/}
-                            <div className="flex items-center justify-center">
-                                <p className="text-white font-semibold fixed">Fazendo</p>
+                            <div className="flex items-center justify-between">
+                                <p className="text-white font-semibold flex-1 text-center">Em progresso</p>
 
                                 <div className="flex justify-end p-1 rounded-[15px]">
                                     {/*icone para minimizar tarefa*/}
-                                    <FiMinimize2 color="white" className="hover:scale-125 ml-[170px]"
+                                    <FiMinimize2 color="white" className="hover:scale-125 cursor-pointer"
                                         onClick={() => toggleMinimize("fazendo")}
                                     />
                                 </div>
@@ -149,11 +148,11 @@ function Lista() {
                         </div>
                         <div className="bg-[#251F1F] text-center p-3 rounded-[5px] flex flex-col w-full h-full gap-y-2 max-w-60">
                             {/*lista de tarefas concluídas*/}
-                            <div className="flex items-center justify-center">
-                                <p className="text-white font-semibold fixed">Concluído</p>
+                            <div className="flex items-center justify-between">
+                                <p className="text-white font-semibold flex-1 text-center">Concluído</p>
                                 <div className="flex justify-end p-1 rounded-[15px]">
                                     {/*icone para minimizar tarefa*/}
-                                    <FiMinimize2 color="white" className="hover:scale-125 ml-[170px]"
+                                    <FiMinimize2 color="white" className="hover:scale-125 cursor-pointer"
                                         onClick={() => toggleMinimize("concluido")}
                                     />
                                 </div>
@@ -187,13 +186,13 @@ function Lista() {
                             <div className="flex items-center justify-center" >
                                 <p className="text-white font-semibold truncate resize-none p-1">{nomelista}</p>
 
-                                    {nomelista && tarefaNovaLista && (
-                                         <div className="flex justify-end p-1 rounded-[15px]">
-                                            {/*icone para minimizar tarefa*/}
-                                            <FiMinimize2 color="white" className="hover:scale-125 ml-[170px]" onClick={() => toggleMinimize("nova")} />
-                                        </div>
-                                    )}
-                                
+                                {nomelista && tarefaNovaLista && (
+                                    <div className="flex justify-end p-1 rounded-[15px]">
+                                        {/*icone para minimizar tarefa*/}
+                                        <FiMinimize2 color="white" className="hover:scale-125 ml-[170px]" onClick={() => toggleMinimize("nova")} />
+                                    </div>
+                                )}
+
 
                             </div>
                             {/*função para minimizar nova lista*/}
@@ -228,7 +227,7 @@ function Lista() {
                                             }}
                                         />)}
 
-                                    {/*botão para criar nova lista*/}    
+                                    {/*botão para criar nova lista*/}
                                     {!nomelista && (
                                         <button id="bnt-lista" className="bg-[#251F1F] text-white text-center  rounded-[5px] p-1  hover:bg-[#493f3f]  w-full cursor-pointer " onClick={() => Setnovalista(true)} >
                                             + Criar nova lista
@@ -251,9 +250,22 @@ function Lista() {
                 </main>
             </div>
 
-            {modaltask && select && (<Modaltaf task={select} onClose={() => Setmodaltask(false)} />)}
-            
-                
+            {modaltask && select && (<Modaltaf task={select} onClose={() => Setmodaltask(false)} onDelete={() => {
+                if (select === tarefapen) Settarefapen("");
+                else if (select === tarefaprog) Settarefaprog("");
+                else if (select === tarefaconc) Settarefaconc("");
+                else if (select === tarefaNovaLista) SettarefaNovaLista("")
+            }}
+                onUpdateTask={(novoNome: string) => {
+                    if (select === tarefapen) Settarefapen(novoNome);
+                    else if (select === tarefaprog) Settarefaprog(novoNome);
+                    else if (select === tarefaconc) Settarefaconc(novoNome);
+                }}
+
+
+            />)}
+
+
         </>
     )
 }
