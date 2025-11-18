2c185df053d89f325148f366964f3f13ac93f377
JoseIlcle14 <joseilclemaia@gmail.com>
2025-11-18 09:45:27 -0300

    url

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import { useAuthStore } from "../store/Auth";
 
 const api = axios.create({
-/*   baseURL: `http://192.168.0.159:3000`
- */  baseURL: `http://127.0.0.1:3000`
-})
+  baseURL: `http://192.168.0.159:3000`
+/*   baseURL: `http://127.0.0.1:3000`
+ */})
 
 
 api.interceptors.response.use((response) => response, (error)=> {
