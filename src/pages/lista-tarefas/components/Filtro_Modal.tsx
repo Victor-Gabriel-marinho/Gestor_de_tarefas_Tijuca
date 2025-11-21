4b7a82209e0e004dc59c27175d77b7905f5e1c25
Victor-Gabriel-marinho <victorgab0802@gmail.com>
2025-11-21 07:33:00 -0300

    ajustando pipline

--- src/pages/lista-tarefas/components/Filtro_Modal.tsx
+++ src/pages/lista-tarefas/components/Filtro_Modal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { FiltroDashboard } from "../../../api/types/DashboardTypes/filtro";
 
 type Filtro_ModalProps = {
